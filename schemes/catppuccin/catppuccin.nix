5b35301716a839c48c00416b7f5dfcc02a35d068
Astro <github@sandy007.anonaddy.com>
2022-01-19 19:33:56 +0530

    update catppuccin colors (#5)

--- schemes/catppuccin/catppuccin.nix
+++ schemes/catppuccin/catppuccin.nix
@@ -3,22 +3,23 @@
   name = "Catppuccin";
   author = "Pocco81 (https://github.com/pocco81)";
   colors = {
-    base00 = "1B1923";
-    base01 = "1E1E28";
-    base02 = "2D293B";
-    base03 = "3E4058";
+    # the official ports use black2 as the background color
+    base00 = "1E1E28";
+    base01 = "1A1826";
+    base02 = "302D41";
+    base03 = "575268";
     base04 = "6E6C7C";
     base05 = "D7DAE0";
-    base06 = "E1EBF5";
-    base07 = "9DDDCB";
-    base08 = "E28C8C";
-    base09 = "F7C196";
-    base0A = "EADDA0";
-    base0B = "B3E1A3";
-    base0C = "F0AFE1";
-    base0D = "A4B9EF";
-    base0E = "C6AAE8";
-    base0F = "ECBFBD";
+    base06 = "F5E0DC";
+    base07 = "C9CBFF";
+    base08 = "F28FAD";
+    base09 = "F8BD96";
+    base0A = "FAE3B0";
+    base0B = "ABE9B3";
+    base0C = "B5E8E0";
+    base0D = "96CDFB";
+    base0E = "DDB6F2";
+    base0F = "F2CDCD";
   };
 }
 
