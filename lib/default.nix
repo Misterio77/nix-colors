c2a8bd140d11acf09c6c794685ece2b7be427336
Gabriel Fontes <eu@misterio.me>
2022-06-10 04:50:07 -0300

    small wording fix

--- lib/default.nix
+++ lib/default.nix
@@ -5,4 +5,4 @@
 # Thus, this is kept for giving a friendlier error. Maybe core lib will move
 # here, eventually, after we've given enough time for people to migrate.
 { ... }: throw ''Opinionated nix-colors functions have been moved from `nix-colors.lib` into `nix-colors.lib-contrib`.
-Please update your `nix-colors.lib { inherit pkgs; }` calls with `nix-colors.lib-contrib { inherit pkgs; }`.''
+Please update your `nix-colors.lib { inherit pkgs; }` calls to `nix-colors.lib-contrib { inherit pkgs; }`.''
