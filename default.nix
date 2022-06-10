a0fe2f460441a928e397b96ec39b5afed92de3e8
Gabriel Fontes <eu@misterio.me>
2022-06-10 01:35:49 -0300

    import schemes from base16-schemes, move functions into contrib

--- default.nix
+++ default.nix
@@ -1,6 +1,25 @@
+{ base16-schemes ? # If not defined (when not using flakes), download with fetchTarball (by lockfile info)
+  let
+    inherit (builtins) fromJSON readFile;
+    inherit ((fromJSON (readFile ./flake.lock)).nodes.base16-schemes) locked;
+  in
+  fetchTarball {
+    url = "https://github.com/${locked.owner}/${locked.repo}/archive/${locked.rev}.tar.gz";
+    sha256 = locked.narHash;
+  }
+, ...
+}:
 rec {
   lib = import ./lib;
-  colorSchemes = import ./schemes;
-  homeManagerModules.colorscheme = import ./module;
+
+  colorSchemes = import ./schemes.nix { inherit lib base16-schemes; };
+
+  homeManagerModules = rec {
+    colorScheme = import ./module;
+    # Aliases
+    colorscheme = colorScheme;
+    default = colorScheme;
+  };
+  # Deprecated alias
   homeManagerModule = homeManagerModules.colorscheme;
 }
--- scripts/nix_to_yaml.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -p nixUnstable -p yq-go -p jq -i bash
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz
-
-nix eval --raw --impure --expr "builtins.toJSON $(tee)" | yq eval '.. style="double"' <( jq '{name: .name, author: .author} * .colors' )
