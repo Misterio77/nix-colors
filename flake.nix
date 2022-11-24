01c5aea7d6724723f3deca4dfde6d916b7bdbe50
Gabriel Fontes <hi@m7.rs>
2022-11-24 10:25:18 -0300

    chore: update flake.lock
    • Updated input 'base16-schemes':
        'github:base16-project/base16-schemes/7c247f734eac7f04518c6e28d098635ee8dcabf5' (2022-06-10)
      → 'github:tinted-theming/base16-schemes/a3dc916cf90471a422c0bfe1bb4b1bdd12185ced' (2022-10-13)

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-lib.url = "github:nix-community/nixpkgs.lib";
 
     # Upstream source of .yaml base16 schemes
-    base16-schemes.url = "github:base16-project/base16-schemes";
+    base16-schemes.url = "github:tinted-theming/base16-schemes";
     base16-schemes.flake = false;
   };
 
